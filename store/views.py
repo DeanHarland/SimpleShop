48c084b3d8608c4edee5a2049d6f6c80ea4a90f8
Dean Harland <mrDeanHarland@gmail.com>
2026-04-20 10:37:10 +0100

    Add product detail view and template; update product list to link to details

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
 # Create your views here.
 
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
 
 def product_list(request):
     products = Product.objects.all()
