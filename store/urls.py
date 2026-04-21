c5c2d8265a155cb76f3a6b4a3e5b05bc28425a02
Dean Harland <mrDeanHarland@gmail.com>
2026-04-21 09:56:02 +0100

    Implement cart functionality with add and remove item features; create cart view template

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:pk>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/', views.cart_view, name='cart'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Product
 # Create your views here.
 
@@ -9,3 +9,56 @@ def product_detail(request, pk):
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'store/product_list.html', {'products': products})
+
+def add_to_cart(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    cart = request.session.get('cart', {})
+
+    if str(pk) in cart:
+        cart[str(pk)]['quantity'] += 1
+    else:
+        cart[str(pk)] = {'quantity': 1}
+        
+
+    request.session['cart'] = cart
+
+    return redirect('product_list')
+
+
+def remove_from_cart(request, pk):
+    cart = request.session.get('cart', {})
+
+    if str(pk) in cart:
+        del cart[str(pk)]
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
+
+
+# def remove_from_cart(request,pk):
+#         product = get_object_or_404(Product, pk=pk)
+#         cart = request.session.get('cart', {})
+
+#         if str(pk) in cart:
+#              cart[str(pk)]['quantity'] -= 1
+#         elif str(pk) in cart and cart[str(pk)]['quantity'] <= 0:
+            
+#         else:
+#              pass
+        
+#         request.session['cart'] = cart
+
+#         return redirect('cart')
+
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    products = []
+
+    for key, value in cart.items():
+        product = Product.objects.get(pk=key)
+        product.quantity = value['quantity']
+        products.append(product)
+
+    return render(request, 'store/cart.html', {'products': products})
